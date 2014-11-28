b0fa00090aba25244c2a20c72946970066017a1b
Daniel Dotsenko <dotsa@hotmail.com>
2014-11-28 10:41:47 -0800

    upgrading python code to python27 runtime

--- gae.py
+++ gae.py
@@ -1,23 +1,13 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+import webapp2
 
-class IndexHandler(webapp.RequestHandler):
-
+class MainPage(webapp2.RequestHandler):
     def get(self):
-        if self.request.url.endswith('/'):
-            path = '%sindex.html'%self.request.url
-        else:
-            path = '%s/index.html'%self.request.url
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
 
-        self.response.headers.add_header('X-UA-Compatible', 'IE=edge,chrome=1')
-        self.redirect(path)
-
-    def post(self):
-        self.get()
-
-def main():
-    application = webapp.WSGIApplication([('/.*', IndexHandler)], debug = False)
-    util.run_wsgi_app(application)
-
-if __name__ == '__main__':
-    main()
+application = webapp2.WSGIApplication(
+    [
+        ('/', MainPage),
+    ],
+    debug=True
+)
